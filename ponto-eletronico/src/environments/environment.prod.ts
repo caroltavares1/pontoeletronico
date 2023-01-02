638fb9957d79b360026b336a9daadf4a306b605a
caroltavares1 <caroltavares.ts@gmail.com>
2023-01-02 14:28:32 -0300

    Preparos para o deploy no Github Pages

--- ponto-eletronico/src/environments/environment.prod.ts
+++ ponto-eletronico/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  title: 'Dev Environment Heading',
+  apiURL: 'http://192.168.41.60:8090/rest',
+  authorization: "Basic " + btoa("admin:")
 };
