0c499d1c0cf99c59123bed71c801f724f38c7a73
Saulo <saulomcm@gmail.com>
2023-01-24 13:55:39 -0300

    Corrige a variavel environment de producao

--- ponto-eletronico/src/environments/environment.prod.ts
+++ ponto-eletronico/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   title: 'Ambiente de Producao',
-  apiURL: 'localhost:8090/rest',
+  apiURL: 'http://lk5.shineray.com.br:8097/rest',
   authorization: "Basic " + btoa("saulomaciel:17072610")
 };
