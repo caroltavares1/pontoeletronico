eec77aad68f23a88f54284295e49cabd1c8b4f59
Saulo Maciel <saulomcm@gmail.com>
2025-11-28 10:12:27 -0300

    fix(env): atualiza url da api de producao para https e novo dominio
    Atualiza a `apiURL` no ambiente de produção para utilizar o protocolo
    seguro (HTTPS) e aponta para o novo domínio `shineraydobrasil.com.br`.
    
    Também incrementa a versão do pacote para 0.0.5.

--- ponto-eletronico/src/environments/environment.prod.ts
+++ ponto-eletronico/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   title: 'Ambiente de Producao',
-  apiURL: 'http://lk5.shineray.com.br:8097/rest',
+  apiURL: 'https://lk5.shineraydobrasil.com.br:8097/rest',
   authorization: "Basic " + btoa("pontoeletronico:Pe@2023#")
 };
